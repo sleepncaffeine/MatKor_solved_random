9ef86c21b04e9d0e83cec135d5892359ca839713
Jinjae Park <parkjinjae0627@gmail.com>
2026-03-12 16:32:05 +0900

    fix cors vercel url

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://*.vercel.app",
+        "https://mat-kor-solved-random.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
